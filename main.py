d2db9138bc0f8d44efbd2d1badc2f538bbbd9220
Mazen Ayyad <mazenayyadwork@gmail.com>
2024-12-24 16:14:35 +0300

    further analysis

--- main.py
+++ main.py
@@ -17,10 +17,44 @@ df = df.rename(columns={'Price' : 'Close', 'Vol.' : 'Volume', 'Change %' : 'Chan
 
 # Convering columns to correct data types
 df['Date'] = pd.to_datetime(df['Date'])
+df['Date'] = df['Date'].dt.date # Removes the time component from datetime (ex: 00:00)
 df['Change_Percentage'] = df['Change_Percentage'].str.replace('%','').astype(float)
 for col in ['Close', 'Open', 'High', 'Low']:
     df[col] = df[col].str.replace(',','').astype(float)
 df['Volume'] = df['Volume'].apply(clean_volume)
 
-df = df.sort_values(by='Date', ascending=False)
-print(df.head(5))
+df = df.sort_values(by='Date', ascending=True)
+
+# Dataset Summary
+maxPriceIndex = df['High'].idxmax()
+maxPrice = df.loc[maxPriceIndex,'High']
+maxPriceDate = df.loc[maxPriceIndex,'Date']
+
+minPriceIndex = df['Low'].idxmin()
+minPrice = df.loc[minPriceIndex, 'Low']
+minPriceDate = df.loc[minPriceIndex, 'Date']
+
+# Note: The 'Volume' column represents trading activity on a specific exchange 
+# and may not accurately reflect the total trading volume of Bitcoin across 
+# all markets, especially given the decline in activity on this particular exchange.
+
+# maxVolumeIndex = df['Volume'].idxmax()
+# maxVolume = df.loc[maxVolumeIndex, 'Volume']
+# maxVolumeDate = df.loc[maxVolumeIndex, 'Date']
+
+# minVolumeIndex = df['Volume'].idxmin()
+# minVolume = df.loc[minVolumeIndex,'Volume']
+# minVolumeDate = df.loc[minVolumeIndex,'Date']
+
+maxChangeIndex = df['Change_Percentage'].idxmax()
+maxChange = df.loc[maxChangeIndex, 'Change_Percentage']
+maxChangeDate = df.loc[maxChangeIndex, 'Date']
+
+minChangeIndex = df['Change_Percentage'].idxmin()
+minChange = df.loc[minChangeIndex, 'Change_Percentage']
+minChangeDate = df.loc[minChangeIndex, 'Date']
+
+print(maxPrice,maxPriceDate,sep=' = ')
+print(minPrice,minPriceDate,sep=' = ')
+print(maxChange,maxChangeDate,sep=' = ')
+print(minChange,minChangeDate,sep=' = ')
